2a86caff1c8fc0e1f2e0e46ffd3df6cabbd63313
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-04-14 15:13:36 +0530

    added : new color added

--- app/(main)/background/page.tsx
+++ app/(main)/background/page.tsx
@@ -13,7 +13,7 @@ const Background = () => {
         <BackgroundColorCard bg="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-900 to-black text-white" />
         <BackgroundColorCard bg="bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]" />
         <BackgroundColorCard bg="bg-[radial-gradient(100%_50%_at_5%_100%,_var(--tw-gradient-stops))] from-blue-700 to-slate-100 text-black" />
-        <BackgroundColorCard bg="bg-[radial-gradient(circle_1000px_at_50%_500px,_var(--tw-gradient-stops))] from-black to-pink-900 text-white" />
+        <BackgroundColorCard bg="bg-[radial-gradient(circle_1000px_at_50%_500px,_var(--tw-gradient-stops))] from-black to-teal-900 text-white" />
         <BackgroundColorCard bg="bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
         <BackgroundColorCard bg="bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_6rem] " />
         {/* <BackgroundColorCard bg="bg-[radial-gradient(circle_800px_at_50%_200px,#d5c5ff,transparent)] " /> */}
