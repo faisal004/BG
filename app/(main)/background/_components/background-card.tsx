87ddaa058ba22e7f81ad7af77bccb23dba2cc04f
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-04-14 15:58:58 +0530

    added : copy functionality added

--- app/(main)/background/_components/background-card.tsx
+++ app/(main)/background/_components/background-card.tsx
@@ -1,23 +1,43 @@
-
-"use client"
+'use client'
 import { Card } from '@/components/ui/card'
-import { useBackgroundStore } from '@/Store/backgroundStore';
+import { useBackgroundStore } from '@/Store/backgroundStore'
+import { Check, Copy } from 'lucide-react'
+import { useState } from 'react'
 
-const BackgroundColorCard = ({bg}:any) => {
-  const { backgroundColor, setBgValue } = useBackgroundStore();
+const BackgroundColorCard = ({ bg }: any) => {
+  const { backgroundColor, setBgValue } = useBackgroundStore()
+  const [copied, setCopied] = useState(false)
+  const handleCopy = () => {
+    navigator.clipboard.writeText(bg)
+    setCopied(true)
+    setTimeout(() => {
+      setCopied(false)
+    }, 1000)
+  }
   const handlePreview = () => {
-    // Set the background value when preview button is clicked
-    setBgValue(bg);
-   
-  };
+    setBgValue(bg)
+  }
   return (
-    <Card className={`h-60 w-full flex items-center  justify-center relative ${bg} `}>
+    <Card
+      className={`h-60 w-full flex items-center  justify-center relative ${bg} `}
+    >
       <div className={`flex absolute top-2 right-2 gap-1`}>
-        <button className="py-0 bg-white rounded-md px-2 text-black h-6 text-xs" onClick={handlePreview}>
+        <button
+          className="py-0 bg-white rounded-md px-2 text-black h-6 text-xs"
+          onClick={handlePreview}
+        >
           Preview
         </button>{' '}
-        <button className="py-0 bg-white rounded-md px-2 text-black h-6 text-xs">
-          Copy
+        <button
+          onClick={handleCopy}
+          disabled={copied}
+          className="py-0 bg-white rounded-md px-2 text-black h-6 text-xs"
+        >
+          {copied ? (
+            <Check className="h-4 w-4" />
+          ) : (
+            <Copy className="h-4 w-4" />
+          )}
         </button>
       </div>
     </Card>
