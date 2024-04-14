080df3affabdf7d875b474c41363cb3ad57b0c7e
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-04-14 14:18:24 +0530

    added : new color added

--- app/(main)/background/_components/background-card.tsx
+++ app/(main)/background/_components/background-card.tsx
@@ -12,7 +12,7 @@ const BackgroundColorCard = ({bg}:any) => {
   };
   return (
     <Card className={`h-60 w-full flex items-center  justify-center relative ${bg} `}>
-      <div className="flex absolute top-2 right-2 gap-1">
+      <div className={`flex absolute top-2 right-2 gap-1`}>
         <button className="py-0 bg-white rounded-md px-2 text-black h-6 text-xs" onClick={handlePreview}>
           Preview
         </button>{' '}
--- app/(main)/background/page.tsx
+++ app/(main)/background/page.tsx
@@ -1,4 +1,3 @@
-import { useBackgroundStore } from '@/Store/backgroundStore'
 import BackgroundColorCard from './_components/background-card'
 
 const Background = () => {
@@ -11,10 +10,13 @@ const Background = () => {
       <div className="grid md:grid-cols-3 grid-cols-1 gap-3 w-full max-w-[1200px] p-2 ">
         <BackgroundColorCard bg="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-500 to-blue-700" />
         <BackgroundColorCard bg="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-violet-900 to-violet-950" />
-        <BackgroundColorCard bg="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-500 to-blue-700" />
-        <BackgroundColorCard bg="bg-[radial-gradient(circle_at_right,_var(--tw-gradient-stops))] from-pink-900 to-pink-800" />
-        <BackgroundColorCard bg="bg-[radial-gradient(100%_5%_at_5%_0%,_var(--tw-gradient-stops))] from-blue-700 to-slate-100 text-black"/>
-        <BackgroundColorCard />
+        <BackgroundColorCard bg="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-900 to-black text-white" />
+        <BackgroundColorCard bg="bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]" />
+        <BackgroundColorCard bg="bg-[radial-gradient(100%_50%_at_5%_100%,_var(--tw-gradient-stops))] from-blue-700 to-slate-100 text-black" />
+        <BackgroundColorCard bg="bg-[radial-gradient(circle_1000px_at_50%_500px,_var(--tw-gradient-stops))] from-black to-pink-900 text-white" />
+        <BackgroundColorCard bg="bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]" />
+        <BackgroundColorCard bg="bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_6rem] " />
+        {/* <BackgroundColorCard bg="bg-[radial-gradient(circle_800px_at_50%_200px,#d5c5ff,transparent)] " /> */}
       </div>
     </div>
   )
