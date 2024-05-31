bfdf9d732f6c4b56fd528f928c0867d7116bad4c
faisal004 <faisalhusain1320@gmail.com>
2024-05-31 19:51:00 +0530

    fix : new style added

--- app/(main)/background/_components/custom-background.tsx
+++ app/(main)/background/_components/custom-background.tsx
@@ -2,7 +2,7 @@
 import { Card } from '@/components/ui/card'
 import { useBackgroundStore } from '@/Store/backgroundStore'
 import useCustomBgStore from '@/Store/customBackgroundStore'
-import { Check, Copy } from 'lucide-react'
+import { Check, Copy, RotateCcw } from 'lucide-react'
 import { useState } from 'react'
 import { convertToUnderscore } from '@/utils/convertToTailwindpos'
 import GradientSelect from './gradient-positon-select'
@@ -11,6 +11,7 @@ import ColorsPercentage from './colors-percentage'
 import ColorsPicker from './colors-picker'
 import { Switch } from '@/components/ui/switch'
 import SlidersComponent from './sliders-components'
+import { Button } from '@/components/ui/button'
 
 const CustomBackground = () => {
   const {
@@ -43,12 +44,13 @@ const CustomBackground = () => {
     colorsPercentage.via
   }% , ${colors.to || 'transparent'}  ${colorsPercentage.to}% )`
 
-
   const backgroundStyle = {
     background: !switchChecked ? gradientNormal : gradientAdvance,
   }
 
-const tailwindtocopy=!switchChecked ? backgroundColorForTailwind : backgroundColorForTailwindAdvance
+  const tailwindtocopy = !switchChecked
+    ? backgroundColorForTailwind
+    : backgroundColorForTailwindAdvance
   const handleCopy = () => {
     navigator.clipboard.writeText(tailwindtocopy)
     setCopied(true)
@@ -65,31 +67,51 @@ const tailwindtocopy=!switchChecked ? backgroundColorForTailwind : backgroundCol
   }
   return (
     <div className="flex md:flex-row flex-col justify-between w-full gap-3">
-      <Card
-        className={`h-80 w-full flex items-center  justify-center relative  `}
-        style={backgroundStyle}
-      >
-        <div className={`flex absolute top-2 right-2 gap-1`}>
-          <button
-            className="py-0 bg-white rounded-md px-2 text-black h-6 text-xs"
-            onClick={handlePreview}
-          >
-            Preview
-          </button>{' '}
-          <button
-            onClick={handleCopy}
-            disabled={copied}
-            className="py-0 bg-white rounded-md px-2 text-black h-6 text-xs"
-          >
-            {copied ? (
-              <Check className="h-4 w-4" />
-            ) : (
-              <Copy className="h-4 w-4" />
-            )}
-          </button>
+      <div className="flex flex-col w-full">
+        <div className=" border-2 border-b-0 rounded-md rounded-b-none px-2 bg-black h-6 flex items-center gap-1">
+          <div className="bg-red-600 h-3 w-3 rounded-full" />
+          <div className="bg-yellow-600 h-3 w-3 rounded-full" />
+
+          <div className="bg-green-600 h-3 w-3 rounded-full" />
         </div>
-        <div>Your Custom Background 😎</div>
-      </Card>
+        <Card
+          className={`h-80 w-full flex items-center  justify-center relative rounded-t-none  `}
+          style={backgroundStyle}
+        >
+          <div className='absolute top-0  w-full text-sm px-2 font-bold border-b-2 '>
+            BG
+          </div>
+          <div className={`flex absolute top-2 right-2 gap-1`}>
+            <button
+              className="py-0 bg-white rounded-md px-2 text-black h-6 text-xs"
+              onClick={handlePreview}
+            >
+              Preview
+            </button>{' '}
+            <button
+              onClick={handleCopy}
+              disabled={copied}
+              className="py-0 bg-white rounded-md px-2 text-black h-6 text-xs"
+            >
+              {copied ? (
+                <Check className="h-4 w-4" />
+              ) : (
+                <Copy className="h-4 w-4" />
+              )}
+            </button>
+          </div>
+          <div className="flex flex-col gap-6 items-center justify-center">
+            <div className="text-center font-bold text-xl tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-black dark:from-white to-gray-600 dark:to-gray-400">
+              Collection of <br />
+              Modern Background
+            </div>
+            <button className="text-[10px] flex items-center gap-1 dark:bg-white bg-black dark:text-black text-white rounded-sm p-1">
+              <RotateCcw className="h-2 w-2" />
+              Reset
+            </button>
+          </div>
+        </Card>
+      </div>
 
       <Card
         className={`h-fit w-full  items-start  justify-center relative p-4 grid grid-cols-3 gap-3   `}
